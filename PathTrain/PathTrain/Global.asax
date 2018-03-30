fea567d40c34c2fb34c71561bc7181b211931c58
Liao_Zhengwen <15114203@bjtu.edu.cn>
2018-03-30 22:07:31 +0800

    Generate the project.

---
+++ PathTrain/PathTrain/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PathTrain.MvcApplication" Language="C#" %>
---
+++ PathTrain/PathTrain/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PathTrain.Startup))]
+namespace PathTrain
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
