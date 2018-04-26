ca2b73244721be860a96a75bd7009c80a4e2afab
lzw37 <lzw37@163.com>
2018-04-26 22:02:35 +0800

    Add some codes for the train-diagram.view.js file.

--- PathTrain/PathTrain/Scripts/TrainDiagram/train-diagram.js
+++ PathTrain/PathTrain/Scripts/TrainDiagram/train-diagram.js
@@ -3,6 +3,7 @@
 var c = document.getElementById("train_diagram");
 var cxt = c.getContext("2d");
 
+
 // Fill the background
 
 cxt.fillStyle = "#FF0000";
@@ -10,25 +11,43 @@ c.clientWidth
 cxt.fillRect(0, 0, c.clientWidth, c.clientHeight);
 
 
-
-
 // Global data handler
 
 function input_data_handler() {
+    var b1 = new Block();
+    frame.blockList.push(b1);
+
     var sta = new Station('0', 'BeijingXi', '0', '0', '0.0');
     model.station_map['0'] = sta;
+    var staView = new StationView(sta, b1, 0);
+    b1.stationViewList.push(staView);
 
     sta = new Station('1', 'Shijiazhuang', '0', '1', '50.0');
     model.station_map['1'] = sta;
+    staView = new StationView(sta, b1, 1);
+    b1.stationViewList.push(staView);
 
     sta = new Station('2', 'ZhengZhou', '0', '2', '80.0');
     model.station_map['2'] = sta;
+    staView = new StationView(sta, b1, 2);
+    b1.stationViewList.push(staView);
+
+    sta = new Station('3', 'WuHan', '0', '3', '110.0');
+    model.station_map['3'] = sta;
+    staView = new StationView(sta, b1, 3);
+    b1.stationViewList.push(staView);
 }
-
-
 
 
 // The main procedure..
 
+frame = new Frame({
+    width: c.clientWidth,
+    height: c.clientHeight,
+});
+
 input_data_handler();
-display();
+
+
+updateView();
+display(cxt);
--- PathTrain/PathTrain/Scripts/TrainDiagram/train-diagram.view.js
+++ PathTrain/PathTrain/Scripts/TrainDiagram/train-diagram.view.js
@@ -7,42 +7,149 @@ function DisplayStyle() {
 
 // The frame of the whole diagram
 
-function Frame() {
+function Frame(size) {
+    this.size = size;  // The size of the canvas client rectangle.
 
-}
+    this.blockList = [];  // The block list.
 
-function Block() {
-    this.margin = 10;  // Margin size for this block in the top and the bottom.
-    this.station_view_list = [];
+    this.margin = {  // Margin parameters of the panorama view.
+        left: 30,
+        right: 30,
+        top: 30,
+        bottom: 30,
+    };
 
-    this.draw = function(){
+    this.totalTimeInMinute = 1440;  // The total length of the time horizon (in minutes).
 
+    // Full diagram rectangle
+    var currentFrame = this;
+    this.innerRectangle = {
+        left : function(){
+            return currentFrame.margin.left;
+        },
+        right : function () {
+            return currentFrame.size.width - currentFrame.margin.left - currentFrame.margin.right;
+        },
+        top : function(){
+            return currentFrame.margin.top
+        },
+        bottom : function () {
+            return currentFrame.size.height - currentFrame.margin.top - currentFrame.margin.bottom
+        }
     }
 
+    // The coordinates of the original point.
+    this.orgPosition = {
+        X: 0,
+        Y: 0,
+    }
+
+    // Transfer parameters between pixels and specific values.
+    this.zoomRatio = {
+        horizontial: 1,  // pixel per minute
+        vertical: 1.2, // pixel per miles
+    } 
+}
+
+
+// Block: a view object of a independent train diagram block
+
+function Block() {
+    this.stationViewList = [];  // Station view list of the block.
+
+    // Position attributes
+    this.margin = 10;  // Margin size for this block in the top and the bottom.
+    this.top = 0;
+    this.bottom = 0;
+    this.left = function () {
+        return frame.orgPosition.X;
+    }
+    this.right = function () {
+        return frame.orgPosition.X + frame.totalTimeInMinute * frame.zoomRatio.horizontial;
+    }
+
+    // update parameters of the current block and its children elements.
+    this.update = function (currentY) {
+        for (var s in this.stationViewList) {
+            this.stationViewList[s].update(currentY);
+        }
+        this.bottom = this.stationViewList[this.stationViewList.length - 1].Y;
+    }
+
+    // draw the block and its elements.
+    this.draw = function (cxt) {
+        for (var s in this.stationViewList) {
+            this.stationViewList[s].draw(cxt);
+        }
+    }
 }
 
 
 // StationView: a view object of the station line in the diagram
 
-function StationView(stationObj) {
+function StationView(stationObj, block, sequence) {
+    // Structure attributes
     this.stationObj = stationObj;
-    this.block = null;
-    this.sequence = null;
-    this.Y_Position = null;
+    this.block = block;
+    this.sequence = sequence;
+    this.milesInBlock = stationObj.miles;
 
+    // Position attributes
+    this.Y = 0;
+    this.left = function () {
+        return this.block.left();
+    }
+    this.right = function () {
+        return this.block.right();
+    }
+
+    // Update the current station view.
+    this.update = function (blockTop) {
+        this.Y = blockTop + frame.zoomRatio.vertical * this.milesInBlock;
+    }
+
+    // Draw the current station view.
+    this.draw = function (cxt) {
+        cxt.moveTo(this.left(), this.Y);
+        cxt.lineTo(this.right(), this.Y);
+        cxt.stroke();
+    }
 }
 
 
 // The main function for reflash the diagram
 
-function display() {
+function display(cxt) {
 
     // test messages.
-
     var d = document.getElementById('info');
     var str = '';
     for (var k in model.station_map) {
         str += model.station_map[k].name + '\<br />';
     }
     d.innerHTML = str;
+
+    // draw diagram.
+    for (var k in frame.blockList) {
+        frame.blockList[k].draw(cxt);
+    }
+
+    // test messages.
+    d.innerHTML = "Finished!";
 }
+
+
+// Update the whole train diagram.
+
+function updateView() {
+    var currentY = frame.innerRectangle.top();
+
+    for (var k in frame.blockList) {
+        var block = frame.blockList[k];
+        block.update(currentY);
+        currentY = block.bottom + block.margin;
+    }
+
+}
+
+
